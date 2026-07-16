1dd5dc5157efb4bb45d4a177cefbcc6b3f0a1d39
debabrata4934-hue <debabrata4934@gmail.com>
2026-07-16 07:35:58 +0530

    feat: create responsive navigation bar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,40 +1,50 @@
 import './Navbar.css';
 
-import { Link } from 'react-router-dom';
-import { FaLaptopCode } from 'react-icons';
+import { Link, NavLink } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { useState } from 'react';
 
 function Navbar(){
 
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const closeMenu = () => setMenuOpen(false);
+
+
   return (
     <>
-      <nav className='navbar'>
+      <header className='navbar'>
+        <div className="container">
 
-        <div className="logo">
-            <FaLaptopCode/>
-            <h2>Tech Monster</h2>
+            <Link to={'/'} className='logo'>
+                {/* Add Logo */}
+                <img src="/logo.png" alt="Tech monster Logo" />
+
+                <div>
+                    <h2>Tech Monster</h2>
+                    <span>Pvt. Ltd.</span>
+                </div>
+            </Link>
+
+            <nav className={menuOpen ? 'nav active' : 'nav'}>
+
+                <NavLink to={'/'} onClick={closeMenu}>Home</NavLink>
+                <NavLink to={'/about'} onClick={closeMenu}>About</NavLink>
+                <NavLink to={'/contact'} onClick={closeMenu}>Contact</NavLink>
+                <NavLink to={'/login'} onClick={closeMenu}>Login</NavLink>
+                
+                <Link to={'/signup'} className='signup-btn' onClick={closeMenu}>
+                    Sign Up
+                </Link>
+            </nav>
+
+            <button className='menu-btn' onClick={() => setMenuOpen(!menuOpen)}>
+                {menuOpen ? <FaTimes /> : <FaBars />}
+            </button>
+
+
         </div>
-
-        <ul>
-            <li>
-                <Link to={'/'}>Home</Link>
-            </li>
-
-            <li>
-                <Link to={'/about'}>About</Link>
-            </li>
-
-            <li>
-                <Link to={'/contact'}>Contact</Link>
-            </li>
-
-            <li>
-                <Link to={'/login'}>Login</Link>
-            </li>
-            <li>
-                <Link to={'/signup'}>Signup</Link>
-            </li>
-        </ul>
-      </nav>
+      </header>
     </>
   )
 
--- frontend/src/pages/Landing/Landing.jsx
+++ frontend/src/pages/Landing/Landing.jsx
@@ -5,6 +5,7 @@ function Landing(){
   return (
     <>
       <Navbar />
+      
       <section>
         Hero Section
       </section>
